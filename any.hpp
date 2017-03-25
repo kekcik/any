44263f543f08521eae0affbbce9e0ee13485d05e
Ivan Trofimov <ivan@trofimov.mobi>
2017-03-25 16:37:13 +0300

    impl

---
+++ any.hpp
@@ -0,0 +1,211 @@
+//
+//  any.hpp
+//  any
+//
+//  Created by Ivan Trofimov on 22.03.17.
+//  Copyright © 2017 Ivan Trofimov. All rights reserved.
+//
+
+#include <cstdlib>
+#include <utility>
+#include <cstdio>
+#include <type_traits>
+
+namespace my_any {
+    template <typename T>
+    static void get_copier(void* dst, void const* src) {
+        new(dst) T(*(T const*) src);
+    }
+    
+    template <typename T>
+    static void get_big_deleter(void *raw) {
+        delete((T*) raw);
+    }
+    
+    template <typename T>
+    static void get_small_deleter(void *raw) {
+        ((T*) raw) -> ~T();
+    }
+    
+    template <typename T>
+    static void get_mover(void *dst, void *src) {
+        new(dst) T(std::move(*(T*) src));
+    }
+    
+    template <typename T>
+    static void* get_allocator() {
+        return new typename std::aligned_storage < sizeof(T), alignof(T) > ::type;
+    }
+}
+
+class any {
+private:
+    static constexpr size_t SMALL_SIZE = 16;
+    size_t status;
+    
+    std::aligned_storage <SMALL_SIZE, SMALL_SIZE> ::type storage;
+    
+    void(*deleter) (void*);
+    void(*copier) (void*, void const*);
+    void(*mover) (void*, void*);
+    void*(*allocator) (void);
+    
+    void clear() {
+        switch (status) {
+            case 1:
+                deleter(& storage);
+                status = 0;
+                break;
+            case 2:
+                deleter(*(void**) & storage);
+                status = 0;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    void * any_raw() const {
+        switch (status) {
+            case 0:
+                return nullptr;
+            case 1:
+                return (void*) & storage;
+            default:
+                return *(void**) & storage;
+        }
+    }
+    
+public:
+    
+    any() {
+        status = 0;
+        storage = {};
+        deleter = [](void*) {};
+        copier = [](void*, void const*) {};
+        mover = [](void*, void*){};
+        allocator = []() -> void* {return nullptr;}; {};
+    }
+    
+    any(any const &other) {
+        status = other.status;
+        deleter = other.deleter;
+        copier = other.copier;
+        mover = other.mover;
+        allocator = other.allocator;
+        if (status == 1) {
+            copier(&storage, &other.storage);
+        } else if (status == 2) {
+            void *tmp = allocator();
+            copier(tmp, *(void**) &other.storage);
+            *(void**) & storage = tmp;
+        }
+    }
+    
+    any(any &&other) {
+        status = other.status;
+        deleter = other.deleter;
+        copier = other.copier;
+        mover = other.mover;
+        allocator = other.allocator;
+        if (status == 1) {
+            mover(&storage, &other.storage);
+        } else if (status == 2) {
+            void * tmp = allocator();
+            mover(tmp, *(void**) & other.storage);
+        }
+    }
+    
+    template < typename T, typename TMP = typename std::enable_if <!std::is_same <std::decay_t<T>, any>::value>::type>
+    any(T&& val) {
+        using T_decay_t = std::decay_t <T> ;
+        if (sizeof(T_decay_t) > SMALL_SIZE) {
+            status = 2;
+            void* tmp = new T_decay_t(std::forward <T> (val));
+            *(void**)& storage = tmp;
+            deleter = my_any::get_big_deleter <T_decay_t> ;
+        } else {
+            status = 1;
+            new(&storage) T_decay_t(std::forward <T> (val));
+            deleter = my_any::get_small_deleter <T_decay_t> ;
+        }
+        copier = my_any::get_copier <T_decay_t>;
+        mover = my_any::get_mover <T_decay_t>;
+        allocator = my_any::get_allocator <T_decay_t>;
+    }
+    
+    any& operator = (any const &other) {
+        any(other).swap(*this);
+        return *this;
+    }
+    
+    any& operator = (any &&other) {
+        any(std::move(other)).swap(*this);
+        return *this;
+    }
+    
+    template <typename T, typename TMP = typename std::enable_if <!std::is_same <std::decay_t <T> , any> ::value> ::type>
+    any& operator = (T&& other) {
+        any(std::forward <T> (other)).swap( * this);
+        return *this;
+    }
+    
+    ~any() {
+        clear();
+    }
+    
+    void swap(any& other) {
+        std::swap(status, other.status);
+        std::swap(storage, other.storage);
+        std::swap(deleter, other.deleter);
+        std::swap(copier, other.copier);
+        std::swap(mover, other.mover);
+        std::swap(allocator, other.allocator);
+    }
+    
+    template <typename T>
+    friend T any_cast(const any& _any);
+    
+    template <typename T>
+    friend T any_cast(any& _any);
+    
+    template <typename T>
+    friend T any_cast(any&& _any);
+    
+    template <typename T>
+    friend T
+    const* any_cast(any const* _any);
+    
+    template <typename T>
+    friend T* any_cast(any* _any);
+};
+
+template <typename T >
+T any_cast(const any &_any) {
+    void * result = _any.any_raw();
+    return *(std::add_const_t < std::remove_reference_t <T>> * ) result;
+}
+
+template <typename T>
+T any_cast(any &_any) {
+    void * result = _any.any_raw();
+    return *(std::remove_reference_t <T> * ) result;
+}
+
+template <typename T>
+T any_cast(any &&_any) {
+    void * result = _any.any_raw();
+    return *(std::remove_reference_t <T> * ) result;
+}
+
+template <typename T>
+T const * any_cast(any const *_any) {
+    const T * result = _any -> any_raw();
+    return result;
+}
+
+template <typename T>
+T * any_cast(any *_any) {
+    T * result = _any -> any_raw();
+    return result;
+}
---
+++ main.cpp
@@ -0,0 +1,137 @@
+//
+//  main.cpp
+//  any
+//
+//  Created by Ivan Trofimov on 22.03.17.
+//  Copyright © 2017 Ivan Trofimov. All rights reserved.
+//
+
+#include <iostream>
+
+#include <iostream>
+#include "any.hpp"
+#include <vector>
+
+void slash() {
+    std::cout << "--------------------------------------------" << std::endl;
+}
+
+struct alignas(128) big {
+    int field;
+    
+    big(int i) {
+        field = i;
+    }
+    
+    big(const big& other) {
+        field = other.field;
+    }
+    
+    big(big&& other) {
+        field = other.field;
+    }
+};
+
+
+void test_1() {
+    any a(3.1415);
+    std::cout << any_cast<double>(a) << '\n';
+}
+
+void test_2() {
+    any b(4.20);
+    b = 1;
+    std::cout << any_cast<int>(b) << '\n';
+}
+
+void test_3() {
+    big d(220);
+    any c(d);
+    std::cout << any_cast<big>(c).field << '\n';
+}
+
+void test_4() {
+    for (int i = 0; i < 100000; i++) {
+        big cc(i);
+        any temp1(cc);
+        any temp2(temp1);
+        std::cout << any_cast<big>(temp2).field << '\n';
+    }
+}
+
+void test_5() {
+    for (int i = 0; i < 100000; i++) {
+        any temp1(i);
+        any temp2(temp1);
+        std::cout << any_cast<big>(temp2).field << '\n';
+    }
+}
+
+void test_6() {
+    any a(true);
+    any b(1ll);
+    std::cout << any_cast<char>(a) << '\n';
+    std::cout << any_cast<double>(b) << '\n';
+    a.swap(b);
+    std::cout << any_cast<char>(b) << '\n';
+    std::cout << any_cast<double>(a) << '\n';
+}
+
+void test_big_big() {
+    std::vector<int> _a;
+    std::string _b = "123";
+    _a.push_back(128);
+    any a(_a), b(_b);
+    a.swap(b);
+    std::cout << any_cast<std::vector<int>>(b)[0] << '\n';
+    std::cout << any_cast<std::string>(a) << '\n';
+    slash();
+}
+
+void test_big_small() {
+    std::vector<int> _a;
+    int _b = 123;
+    _a.push_back(128);
+    any a(_a), b(_b);
+    a.swap(b);
+    std::cout << any_cast<std::vector<int>>(b)[0] << '\n';
+    std::cout << any_cast<int>(a) << '\n';
+    slash();
+}
+
+void test_small_big() {
+    std::vector<int> _a;
+    int _b = 123;
+    _a.push_back(128);
+    any a(_a), b(_b);
+    b.swap(a);
+    std::cout << any_cast<std::vector<int>>(b)[0] << '\n';
+    std::cout << any_cast<int>(a) << '\n';
+    slash();
+}
+
+void test_small_small() {
+    double _a = 128.0;
+    int _b(123);
+    any a(_a), b(_b);
+    b.swap(a);
+    std::cout << any_cast<double>(b) << '\n';
+    std::cout << any_cast<int>(a) << '\n';
+    slash();
+}
+
+
+int main() {
+    
+    test_1();
+    test_2();
+    test_3();
+    test_4();
+    test_5();
+    test_6();
+    test_big_big();
+    test_small_big();
+    test_big_small();
+    test_small_small();
+    
+}
